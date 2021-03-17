c538adedd520ce81f7b8d2ae4de457c646bec31f
ed100miles <ed100miles@gmail.com>
2021-03-17 20:20:39 +0000

    predictor update - no strftime, unixT for CSV model build update

--- model.py
+++ model.py
@@ -1,40 +1,76 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow import keras
 import numpy as np
+import time
+import os
+import datetime
 
-X = np.load('X.npy', 'r')
-y = np.load('y.npy', 'r')
+X = np.load('/content/drive/MyDrive/X.npy', 'r')
+y = np.load('/content/drive/MyDrive/y.npy', 'r')
 
-nodes = 64
-
+conv_layers = [3, 4, 5]
+conv_nodes = [256, 512]
+lin_layers = [0, 1, 2]
+lin_nodes = [16, 32]
+batch_sizes = [256]
+epochs = 30
+# 0.0005 got 33.36 loss, up to 88% acc
+# 0.0002 val_loss: 0.3156 - val_accuracy: 0.8682
+# 0.0005 val_loss: 0.2902 - val_accuracy: 0.8726
+learning_rate = 0.0005
 X = X/255.0
 
-model = Sequential()
+for conv_layer in conv_layers:
+  model = Sequential()
+  for conv_node in conv_nodes:
+    for lin_layer in lin_layers:
+        for lin_node in lin_nodes:
+          for batch_size in batch_sizes:
+            for layer in range(conv_layer):
+                model.add(Conv2D(conv_node, (2,2), input_shape = X.shape[1:]))
+                model.add(Activation('relu'))
+                if layer < 2:
+                  model.add(MaxPooling2D(pool_size=(2,2)))
+            model.add(Flatten())
+            for layer2 in range(lin_layer):
+                model.add(Dense(lin_node))
+                model.add(Activation('relu'))
 
-model.add(Conv2D(64, (3,3), input_shape = X.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+            # output layer
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+            model_runtime = int(time.time())
+            model_name = (f'CatSpot-Conv:{conv_layer}*{conv_node}-Linear:{lin_layer}*{lin_node}-@{model_runtime}')
 
-model.add(Flatten())
+            opt = keras.optimizers.Adam(learning_rate=learning_rate)
 
-model.add(Dense(nodes))
-model.add(Activation('relu'))
+            model.compile(loss='binary_crossentropy',optimizer=opt ,metrics=['accuracy'])
 
-model.add(Dense(nodes))
-model.add(Activation('relu'))
+            print('\nConv_layers:', conv_layer, '\nConv_nodes:', conv_node, '\nBatch_size: ', batch_size, '\nRuntime:', model_runtime)
+            # '\nLin_layers:', lin_layer, '\nLin_nodes:', lin_node, 
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+            logdir = os.path.join("/content/drive/MyDrive/logs", model_name)
 
-model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+            tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
 
-model.fit(X, y, batch_size=16, epochs=1, validation_split=0.1)
+            model_struct = f'CatSpot-Conv:{conv_layer}*{conv_node}-Linear:{lin_layer}*{lin_node}'
 
-model.save('catSpot.model')
+            checkpoint_filepath = '/content/drive/MyDrive/checkpoints/' + model_struct + '-V_Loss:{val_loss:.4f}-V_Acc:{val_accuracy:.4f}'
+            
+            model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+                filepath=checkpoint_filepath,
+                save_weights_only=False,
+                monitor='val_loss',
+                mode='min',
+                save_best_only=True)
 
-# prediction = model.predict(X[:10])
+            early_stop_callback = tf.keras.callbacks.EarlyStopping(
+                monitor='val_loss', min_delta=0, patience=3, verbose=0,
+                mode='auto', baseline=None)
 
-# print(prediction)
-# print(y[:10])
+            callbacks_list = [tensorboard_callback,
+                              early_stop_callback]
 
+            model.fit(X, y, batch_size=batch_size, epochs=epochs, validation_split=0.1, callbacks=[callbacks_list])
--- predictor.py
+++ predictor.py
@@ -7,34 +7,77 @@ from datetime import datetime
 from time import sleep
 
 img_size =  75
+img_path = '/home/ed/imgs/motion/motion.jpg'
+
+prediction_thresh = 0.7
+
+model_one = 'CatSpot-ModelNo_1__5_512-V_Loss_0.2573-V_Acc_0.8903'
+model_two = 'CatSpot-ModelNo_3__5_512-V_Loss_0.2718-V_Acc_0.8875'
+model_three = 'CatSpot-ModelNo_2__5_512-V_Loss_0.2731-V_Acc_0.8863'
+
+now = datetime.now()
+strTime = now.strftime("%d-%m-%Y__%H:%M:%S")
+
+predictorLog = open("predictorLog.txt", "a")
+predictorLog.write(f"START: {strTime}")
+predictorLog.close()
+
+def predictor(img_path):
+    predictions = []
+    now = datetime.now()
+    strTime = now.strftime("%d-%m-%Y__%H:%M:%S")
+    predictorLog = open("predictorLog.txt", "a")
+    predictorLog.write(f"Motion detected {strTime} seeing if it's a cat...\n")
+    sleep(5)
+    img_array = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array,(img_size, img_size))
+    X = [new_array]
+    X = np.array(X).reshape(-1, img_size, img_size, 1)
+    X = X/255.0
+
+    model = tf.keras.models.load_model(model_one)
+    prediction = model.predict([X])
+    predictions.append(prediction[0][0])
+    predictorLog.write(f'Model 1 prediction = {prediction}\n')
+
+    model = tf.keras.models.load_model(model_two)
+    prediction = model.predict([X])
+    predictions.append(prediction[0][0])
+    predictorLog.write(f'Model 2 prediction = {prediction}\n')
+
+    model = tf.keras.models.load_model(model_three)
+    prediction = model.predict([X])
+    predictions.append(prediction[0][0])
+    predictorLog.write(f'Model 3 prediction = {prediction}\n')
+
+    avg_prediction = (predictions[0] + predictions[1] + predictions[2]) / 3
+    predictorLog.write(f'Average prediction = {avg_prediction}\n')
+    predictorLog.close()
+
+    return avg_prediction
 
 while True:
-        now = datetime.now()
-        now_string = now.strftime("%d-%m-%Y__%H:%M:%S")
-        print(now_string, ' :: monitoring...')
+    #now = datetime.now()
+    # now_string = now.strftime("%d-%m-%Y__%H:%M:%S")
+    # print(now_string, ' :: monitoring...')
 
-        if os.path.exists('/home/ed/imgs/motion/motion.jpg'):
-                model = tf.keras.models.load_model('catSpot.model')
-                print("motion detected, seeing if it's a cat...")
-                sleep(2)
-                img_array = cv2.imread('/home/ed/imgs/motion/motion.jpg', cv2.IMREAD_GRAYSCALE)
-                new_array = cv2.resize(img_array,(img_size, img_size))
-                X = [new_array]
-                X = np.array(X).reshape(-1, img_size, img_size, 1)
-                X = X/255.0
-                model = tf.keras.models.load_model('catSpot.model')
-                prediction = model.predict([X])
-                print(prediction)
-                if prediction[0][0] > 0.65:
-                        now = datetime.now()
-                        spotTime = now.strftime("%d-%m-%Y__%H:%M:%S")
-                        print(f'found cat at {spotTime}')
-                        os.system('mv /home/ed/imgs/motion/motion.jpg /home/ed/gitcode/Portfolio/static/imgs/catSpots/lastCatSpot.jpg')
-                        with open('/home/ed/gitcode/Portfolio/static/csvs/catSpots.csv', 'a') as csvfile:
-                                csvfile.write(spotTime + ',')
-                else:
-                        print("that's no cat...")
-                        os.system('rm /home/ed/imgs/motion/motion.jpg')
+    if os.path.exists(img_path):
+        sleep(5)
+        avg_prediction = predictor(img_path)
+        if avg_prediction > prediction_thresh:
+            now = datetime.now()
+            # spotTime = now.strftime("%d-%m-%Y__%H:%M:%S")
+            predictorLog = open("predictorLog.txt", "a")
+            predictorLog.write(f"Found cat at {now}")
+            predictorLog.close()
+            os.system(f'cp {img_path} /home/ed/imgs/spots/{now}.jpg')
+            os.system(f'mv {img_path} /home/ed/gitcode/Portfolio/static/imgs/catSpots/lastCatSpot.jpg')
+            
+            with open('/home/ed/gitcode/Portfolio/static/csvs/catSpots.csv', 'a') as csvfile:
+                    csvfile.write(f'{now},{avg_prediction}\n')
+                    csvfile.close()
         else:
-                sleep(10)
+            print("that's no cat...")
+            os.system(f'rm {img_path}')
+
 
